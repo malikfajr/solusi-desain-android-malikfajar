ce2c030bd0333607718e1de96d1c8b63b22ca796
ThemBig <malilfajar17@gmail.com>
2020-10-14 10:50:55 +0700

    basic function

---
+++ basicFunction/hello.kt
@@ -0,0 +1,14 @@
+fun hello(name: String = "") :String {
+  if (name.length == 0) {
+    return "Hello, World!"
+  } else {
+    return "Hello, ${name.toLowerCase().capitalize()}!"
+  }
+}
+
+fun main() {
+  println(hello("john"))
+  println(hello("aliCe"))
+  println(hello(""))
+  println(hello())
+}
